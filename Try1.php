358ce0f011bff69ff73538497f80c8bad860be52
Richard Freitas <richardgfreitasx@gmail.com>
2018-07-18 22:34:12 -0300

    Try1

---
+++ Try1.php
@@ -0,0 +1,77 @@
+<html>	<!-- Podia ser php -->
+<head>	<!-- Cabaralho -->
+		<meta charset="UTF=8"/>  <!-- UTF=8 é pra evitar bugs -->
+		<title>Document</title>
+</head>
+<body>	<!-- Códs -->
+	<h1>Título 1</h1>  <!-- De 1 a 6 na escala Richter-->
+	<h2>Título 2</h2>
+	<h3>Título 3</h3>
+
+	<p>Parágrafo 1</p> <!-- Parágrafo tem acento pq é proparoxítona -->
+	<p>Parágrafo 2</p>
+	<p>Parágrafo 3</p>
+
+	<img src="imagem.jpg" alt="Num vai da" title="Texto da img"> <!-- src pra link ou local da img, alt proq vai aparecer sem a img n funfar, title pra aparecer um txt qnd parar o mouse em cima -->
+
+	<br/>
+
+	<a href="#h3">Go to h3</a> <!-- serve pra botar link -->
+
+	<table> <!-- pra fazer lista -->
+			<tr> <!-- pra criar uma linha -->
+				<td>Linha 1, Coluna 1 </td> <td>Coluna 2 </td> <td>Coluna 3 </td>
+				 <!-- Digita na linha --> 
+			</tr>
+			<tr>
+				<td>Linha 2, Coluna 1 </td> <td>Coluna 2 </td> <td>Coluna 3 </td>
+				 <!-- Mais um pra outra linha --> 
+			</tr>	
+	</table>
+
+
+	<dl>	<!-- Lista de Definição-->
+    	<dt>Título 1</dt>  <!-- Título da Definição-->
+    		<dd>Definição 1</dd> <!-- Definição-->
+    	<dt>Título 2</dt>
+    		<dd>Definição 2</dd>
+    	<dt>Título 3</dt>
+    		<dd>Definição 3</dd>
+	</dl>
+
+	<form action="salvar_dados.php" method="post">
+		<input type="text" placeholder="Escreva aqui"> <br/>
+		<input type="checkbox"> Skrrr <br/>
+		<input type="radio"> Butaum <br/>
+		<input type="range"> <br/>
+		<input type="submit" value="Enviar"> Otro butaum <br/>
+	</form>
+
+	<p><b>Negrito é assim.<b></p>
+	<p><i>Itálico é assim.</i></p>
+	<p><sup>Sobrescrito é assim.</sup></p>
+	<p><sub>Subscrito é assim. <sub/></p>
+	<p><ins>Sublinhado é assim.</ins></p>
+	<p><del>Excluído é assim.</del></p>
+	<p><small>Pequeno é assim</small></p>
+	<p><mark>E destacado é assim<mark/></p>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
